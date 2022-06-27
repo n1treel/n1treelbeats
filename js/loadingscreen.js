fd2641d6993981c868ff40ae726869e529f53fcd
Evgenii <pokebrosok@gmail.com>
2022-06-27 20:29:01 +0300

    N1TREELBEATS V2

---
+++ js/loadingscreen.js
@@ -0,0 +1,8 @@
+let spinnerWrapper = document.querySelector('.spinner-wrapper');
+
+
+window.addEventListener('load', function () {
+    ///document.querySelector('.spinner-wrapper').classList.add('fadeOut');
+    setTimeout(() => {spinnerWrapper.style.display = 'none'; }, 2500);
+    
+});
